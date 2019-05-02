3180bf7652fc950f66675f5a1ab5a4fd6d3aa1d1
muhammad <muhammad@g3t.co.uk>
2019-05-02 16:17:34 +0100

    Recipe routes  update

--- core/classes/Recipe.js
+++ core/classes/Recipe.js
@@ -1,4 +1,48 @@
 class Recipe {
+    /**
+     * All Recipes
+     */
+    static getAllRecipesSQL() {
+            let sql = `SELECT * FROM meal_course
+                JOIN course_details
+                ON meal_course.id = course_details.course_id
+                WHERE meal_course.meal_type =2`;
+            return sql;
+        }
+        /**
+         *  Recipes ByID
+         */
+    static getAllRecipeByIDSQL(id) {
+            let sql = `SELECT * FROM meal_course
+                JOIN course_details
+                ON meal_course.id = course_details.course_id
+                WHERE meal_course.id =${id}`;
+            return sql;
+        }
+        /**
+         * All Veg Recipes
+         */
+    static getAllVegRecipeSQL() {
+            let sql = `SELECT * FROM meal_course
+        JOIN course_details
+        ON meal_course.id = course_details.course_id
+        WHERE meal_course.meal_type =2
+        AND meal_course.course_type =1`;
+            return sql;
+        }
+        /**
+         * All Non-Veg Recipes
+         */
+    static getAllNonVegRecipeSQL() {
+        let sql = `SELECT * FROM meal_course
+    JOIN course_details
+    ON meal_course.id = course_details.course_id
+    WHERE meal_course.meal_type =2
+    AND meal_course.course_type =2`;
+        return sql;
+    }
+
+
 
 }
 
