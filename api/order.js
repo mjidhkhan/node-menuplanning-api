c197ccdbeb0c8a75981e757b37256461bc2af32b
MHK <mhk@Majids-MacBook-Pro.local>
2019-05-02 05:42:51 +0100

    adding base

---
+++ api/order.js
@@ -0,0 +1,7 @@
+import db from "../db/database";
+
+import MealType from '../core/classes/MealType';
+import action from '../core/process/process';
+
+const router = express.Router();
+module.exports = router;
---
+++ api/starter.js
@@ -0,0 +1,8 @@
+import db from "../db/database";
+
+import MealType from '../core/classes/MealType';
+import action from '../core/process/process';
+
+const router = express.Router();
+
+module.exports = router;
