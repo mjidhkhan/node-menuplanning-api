4b4fc2678206844d32a258d04129d2833e5a5cec
MHK <mhk@Majids-MacBook-Pro.local>
2019-05-02 06:09:32 +0100

    messages text change

--- api/coursetype.js
+++ api/coursetype.js
@@ -47,8 +47,8 @@ router.put("/update", (req, res, next) => {
     var cid = req.body.id;
     let coursetype = new CourseType(req.body.meal_type);
     db.query(coursetype.updateCourseTypeSQL(mid), (err, data) => {
-        var success = `Meal Type Updated.`;
-        var fail = `Meal Type Not found with id = ${cid}. ${err}`;
+        var success = `Course Type Updated.`;
+        var fail = `Course Type Not found with id = ${cid}. ${err}`;
         action.Update(err, data, res, cid, success, fail);
     });
 });
@@ -59,8 +59,8 @@ router.put("/update", (req, res, next) => {
 router.post("/delete", (req, res, next) => {
     var cid = req.body.id;
     db.query(MealType.deleteCourseTypesSQL(mid), (err, data) => {
-        var success = `Meal Type  deleted with id = ${cid}.`;
-        var fail = "Meal Type Not Found";
+        var success = `Course Type  deleted with id = ${cid}.`;
+        var fail = "Course Type Not Found";
         action.Delete(err, data, res, cid, success, fail);
     })
 });
