a04654a390418906731c0667113cabb46e417313
MHK <mhk@Majids-MacBook-Pro.local>
2019-05-02 05:50:54 +0100

    add functionality

--- core/classes/CourseType.js
+++ core/classes/CourseType.js
@@ -1,5 +1,52 @@
 class CourseType {
 
+    constructor(course_type) {
+        this.id = 0;
+        this.type = course_type
+    }
+    /**
+     * Get All Course Types
+     */
+    static getAllCourseTypesSQL() {
+        let sql = `SELECT * FROM course_type`;
+        return sql;
+    }
+
+    /**
+     *  Delete Course Type
+     * @param {*} id 
+     */
+    static deleteCourseTypesSQL(id) {
+        let sql = `DELETE FROM course_type WHERE id = ${id}`;
+        return sql;
+    }
+
+    /**
+     * Get Course Type ByID
+     * @param {*} id 
+     */
+    static getCourseTypeByIdSQL(id) {
+        let sql = `SELECT * FROM course_type WHERE id =${id}`
+        return sql;
+    }
+
+    /**
+     * Add Course Type
+     */
+    addCourseTypeSQL() {
+        let sql = `INSERT INTO meal_type (course_type) VALUES('${this.type}')`;
+        return sql;
+    }
+
+    /**
+     * Update Course Type
+     */
+    updateCourseTypeSQL(id) {
+        let sql = `UPDATE course_type SET course_type = '${this.type}' WHERE id =${id}`;
+        return sql;
+    }
+
 }
+
 
 export default CourseType;
