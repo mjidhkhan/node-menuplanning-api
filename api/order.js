0b530355223faa9091edc68f5ec10ce1f97d0926
muhammad <muhammad@g3t.co.uk>
2019-05-07 12:50:46 +0100

    Orders  Route add and   update

--- api/order.js
+++ api/order.js
@@ -5,4 +5,15 @@ import Order from '../core/classes/Order';
 import action from '../core/utils/utils';
 
 const router = express.Router();
+/**
+ * Get Desserts
+ */
+router.get("/", (req, res, next) => {
+
+    db.query(Order.AllOrdersSQL(), (err, data) => {
+        let msg = "Desserts listed."
+        action.All(err, data, res, msg);
+    });
+
+});
 module.exports = router;
