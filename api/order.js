11b3142c1d351efacf8b0a7e23c491e9cc11cfe2
muhammad <muhammad@g3t.co.uk>
2019-05-07 14:09:23 +0100

    Orders  Route add and   update

--- api/order.js
+++ api/order.js
@@ -1,19 +1,31 @@
 import express from "express";
 import db from "../db/database";
 
-import Order from '../core/classes/Order';
+import Orders from '../core/classes/Order';
 import action from '../core/utils/utils';
 
 const router = express.Router();
 /**
- * Get Desserts
+ * Get All Orders
  */
 router.get("/", (req, res, next) => {
 
-    db.query(Order.AllOrdersSQL(), (err, data) => {
-        let msg = "Desserts listed."
+    db.query(Orders.AllOrdersSQL(), (err, data) => {
+        let msg = "Orders listed."
         action.All(err, data, res, msg);
     });
 
+});
+/**
+ * Get Orders ByID
+ */
+router.get("/:itemId", (req, res, next) => {
+    let pid = req.params.itemId;
+
+    db.query(Orders.OrderByIDSQL(pid), (err, data) => {
+        var success = "Order found.";
+        var fail = "Order Not found. with  ID: " + pid;
+        action.ById(err, data, res, pid, success, fail);
+    });
 });
 module.exports = router;
--- core/classes/Order.js
+++ core/classes/Order.js
@@ -9,9 +9,9 @@ class Orders {
         /**
          * All Main Courses
          */
-    static OrderByIDSQL() {
+    static OrderByIDSQL(pid) {
             var sql = this.AllOrdersSQL()
-            sql += `WHERE orders.order.id = 2`;
+            sql += ` WHERE orders.id = ${pid}`;
             return sql;
         }
         /**
@@ -37,26 +37,15 @@ class Orders {
 
 
     static allQuery() {
-        let sql = `SELECT 
-            order_details.order_id,
-            orders.customer_id,
-            orders.order_date,
-            orders.booking_date,
-            users.fullname,
-            users.email,
-            order_details.course_name,
-            meal_type.meal_type,
-            course_type.course_type,
-            order_details.servings
+        let sql = `SELECT  order_details.order_id,orders.order_date, orders.booking_date,
+            orders.customer_id,users.fullname,users.email,
+            order_details.course_id,order_details.course_name, 
+            meal_type.meal_type,course_type.course_type, order_details.servings
             FROM orders 
-            JOIN order_details 
-            ON orders.id = order_details.order_id
-            JOIN users 
-            ON users.id = orders.customer_id
-            join meal_type
-            on meal_type.id = order_details.meal_type
-            JOIN course_type
-            ON course_type.id =order_details.course_type`;
+            JOIN order_details      ON orders.id = order_details.order_id
+            JOIN users              ON users.id = orders.customer_id
+            JOIN meal_type          ON meal_type.id = order_details.meal_type
+            JOIN course_type        ON course_type.id = order_details.course_type`;
         return sql;
     }
 
