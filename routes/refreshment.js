58644b237e53c11dbd441bb47bd89ffc4a7d42fb
muhammad <muhammad@g3t.co.uk>
2019-05-08 09:23:11 +0100

    Refreshment route added

--- routes/refreshment.js
+++ routes/refreshment.js
@@ -5,4 +5,30 @@ import Refreshment from '../core/classes/Refreshment';
 import action from '../core/utils/utils';
 
 const router = express.Router();
+
+/**
+ * Get Refreshments
+ */
+router.get("/", (req, res, next) => {
+
+    db.query(Desserts.getAllDessertsSQL(), (err, data) => {
+        let msg = "Desserts listed."
+        action.All(err, data, res, msg);
+    });
+
+});
+
+
+/**
+ * Get Refreshment ByID
+ */
+router.get("/:itemId", (req, res, next) => {
+    let pid = req.params.itemId;
+    console.log(pid)
+    db.query(Desserts.getDessertByIdSQL(pid), (err, data) => {
+        var success = "Starters found.";
+        var fail = "Starters Not found.";
+        action.ById(err, data, res, pid, success, fail);
+    });
+});
 module.exports = router;
